6ee6b131eb719a3a26b50bf738f0a50532ed5b4d
Vitaliy <vitalianobro@gmail.com>
2026-03-21 11:12:42 +0300

    Добавил посты в most_popular_posts

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
 
+def get_likes_count(post):
+    return post.likes.count()
+
+
 def get_related_posts_count(tag):
     return tag.posts.count()
 
@@ -28,8 +32,10 @@ def serialize_tag(tag):
 
 
 def index(request):
-
-    most_popular_posts = []  # TODO. Как это посчитать?
+    post = Post.objects.all()
+    popular_posts = sorted(post, key=get_likes_count)
+    most_popular_posts = popular_posts[-5:]
+    
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -79,7 +85,7 @@ def post_detail(request, slug):
     popular_tags = sorted(all_tags, key=get_related_posts_count)
     most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = []
 
     context = {
         'post': serialized_post,
